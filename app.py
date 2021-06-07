f1036b8f5961960283480e67e20c20cb9182d432
Rohan K Nandula <19r11a0581@gcet.edu.in>
2021-06-07 16:02:54 +0530

    SNS Integration and chart changes

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
-#from flask_mail import Mail, Message
 import requests
 import json
 import random
 import time
 from datetime import datetime
 
-#Wallet balance API: https://ss979hyehb.execute-api.ap-south-1.amazonaws.com/WalletBalance
+
 app = Flask(__name__)
 random.seed()  # Initialize the random number generator
-#app.config['MAIL_SERVER']='smtp.gmail.com'
-#app.config['MAIL_PORT'] = 465
-#app.config['MAIL_USERNAME'] = 'nbs1969test@gmail.com'
-#app.config['MAIL_PASSWORD'] = 'Rohnik@12'
-#app.config['MAIL_USE_TLS'] = False
-#app.config['MAIL_USE_SSL'] = True
 
-#mail = Mail(app)
 
 # Function to add a specific expense for a user - AWS API Add Expense for a user
 def setExpenses(params): 
@@ -79,43 +71,19 @@ def wallet():
 # Function to fetch current wallet balance - AWS API Wallet Balance
 @app.route('/dashboard')
 def dashboard():
-    user = "nbalu@gmail.com"
+    user = "nbalu@gmail.com" #hardcoded as "user session" is not implemented
     params="user="+user
     data=walletBalance(params)
     print(type(data))
-   # sendSMS = False
-   # sendEmail = False
-    if int(data) == 0:
-        message = "Critical!: Wallet current balance. Please add money to wallet"
-       # sendSMS = True
-       # sendEmail = True
-    elif int(data) < 5000:
-        message = "Warning: Wallet current balance is less than threshold limit"
-        #sendSMS = True
-        #sendEmail = True
-    else:
-        message = "Your current wallet balance is above threshold limit"
-    
-    # Send an SMS for case where sendSMS is true
-    #phone = '8008275558'
-    #if sendSMS:
-       # url="https://www.fast2sms.com/dev/bulk?authorization=xCXuwWTzyjOD2ARd1EngbH3a7tKIq5PklJ8YSf0Lh4FQZecs9iNI1dSvuqprxFwCKYJXA5amQkBE36Rl&sender_id=FSTSMS&message="+message+"&language=english&route=p&numbers="+str(phone)
-       # result=requests.request("GET",url)
-       # print(result)
-    # Send an Email for case where sendEmail is true
-    #if sendEmail:
-        #if app.config['MAIL_USERNAME'] == '' or app.config['MAIL_PASSWORD'] == '':
-           # print("Admin email or password is not configured.")
-       # else:
-           # msg = Message('Hello', sender = app.config['MAIL_USERNAME'], recipients = ['nbalas1969@gmail.com'])
-           # msg.body = message
-           # mail.send(msg)
-           # print("Mail sent succesfully to customer!")
-
-    if('errorType' in data):
+    result = data.split("+")
+    wbalance=result[0]
+    message=result[1]
+    print(wbalance,message)    
+       
+    if('errorType' in result):
             return render_template('login.html', pred="Wallet could not be updated. Your wallet balance has not changed.")
     else:
-            return render_template('dashboard.html', pred=data, message=message)
+            return render_template('dashboard.html', pred=wbalance, message=message)
 
 # Function to add amount to wallet - AWS API Add money to wallet
 @app.route('/addmoneypage', methods=['GET','POST'])
@@ -135,48 +103,46 @@ def addmoneypage() :
         return render_template('addmoney.html', pred="Wallet has been updated successfully and your "+data)
     
 
-@app.route("/chart")
+@app.route("/chart", methods=['GET','POST'])
 def chart():
-    if request.method == "POST":
-        user='nbalu@gmail.com'
-        expense_date_from=''
-        expense_date_to=''
-        params = "user="+user+"&expense_date_from="+expense_date_from+"&expense_date_to="+expense_date_to
-        print(params)
-        series_new=fetchExpenses(params)
-        print(type(series_new))
-        exp_dates = []
-        for d in series_new:
-            for k,v in d.items():
-                if k == 'expense_date':
-                    exp_dates.append(v)
-        print(exp_dates)
-        home_expenses = []
-        for d in series_new:
-            for k,v in d.items():
-                if k == 'home_expenses':
-                    home_expenses.append(int(v))
-        print(home_expenses)
-        medical_expenses = []
-        for d in series_new:
-            for k,v in d.items():
-                if k == 'medical_expenses':
-                    medical_expenses.append(int(v))
-        print(medical_expenses)
-        vehicle_expenses = []
-        for d in series_new:
-            for k,v in d.items():
-                if k == 'vehicle_expenses':
-                    vehicle_expenses.append(int(v))
-        print(vehicle_expenses)
-        legend1 = 'Home Expenses'
-        legend2 = 'Vehicle Expenses'
-        legend3 = 'Medical Expenses'
-        #labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        labels = exp_dates      
-        #values = [1000, 9000, 8000, 5000, 6000, 4000, 7000, 8000, 3000, 5500, 9000, 5000]       
-        
-        return render_template('chart.html', labels=labels, legend1=legend1, legend2=legend2, legend3=legend3, home_expenses=home_expenses, vehicle_expenses= vehicle_expenses, medical_expenses=medical_expenses )
+    
+    user='nbalu@gmail.com'
+    expense_date_from=''
+    expense_date_to=''
+    params = "user="+user+"&expense_date_from="+expense_date_from+"&expense_date_to="+expense_date_to
+    print(params)
+    series_new=fetchExpenses(params)
+    print(type(series_new))
+    exp_dates = []
+    for d in series_new:
+        for k,v in d.items():
+            if k == 'expense_date':
+                exp_dates.append(v)
+    print(exp_dates)
+    home_expenses = []
+    for d in series_new:
+        for k,v in d.items():
+            if k == 'home_expenses':
+                home_expenses.append(int(v))
+    print(home_expenses)
+    medical_expenses = []
+    for d in series_new:
+        for k,v in d.items():
+            if k == 'medical_expenses':
+                medical_expenses.append(int(v))
+    print(medical_expenses)
+    vehicle_expenses = []
+    for d in series_new:
+        for k,v in d.items():
+            if k == 'vehicle_expenses':
+                vehicle_expenses.append(int(v))
+    print(vehicle_expenses)
+    legend1 = 'Home Expenses'
+    legend2 = 'Vehicle Expenses'
+    legend3 = 'Medical Expenses'
+    labels = exp_dates             
+    
+    return render_template('chart.html', labels=labels, legend1=legend1, legend2=legend2, legend3=legend3, home_expenses=home_expenses, vehicle_expenses= vehicle_expenses, medical_expenses=medical_expenses )
  
 @app.route('/login')
 def login():
@@ -244,9 +210,6 @@ def register():
 	    city = request.form.get('city')
 	    occupation = request.form.get('occupation')
 	    passw = request.form.get('passw')
-	    #passparam = hash_password(passw)
-	    #x = [x for x in request.form.values()]
-	    #params = "name="+x[0]+"&user="+x[1]+"&phone="+x[2]+"&city="+x[3]+"&occupation="+x[4]+"&passw="+x[5]
 	    params="name="+name+"&user="+user+"&phone="+phone+"&city="+city+"&occupation="+occupation+"&passw="+passw
 	    if('errorType' in check(user)):
 	    	url = "https://nqwsosw3ag.execute-api.ap-south-1.amazonaws.com/QA?"+params
@@ -261,4 +224,4 @@ def logout():
    return redirect(url_for('login'))        
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',port='5000')
